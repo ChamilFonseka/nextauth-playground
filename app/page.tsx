2507517ebb558000560edad698a42b2aba278d7e
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-11-24 17:43:48 +0530

    implemented the login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
+import { Poppins } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button';
+import { LoginButton } from '@/components/auth/login-button';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ["600"],
+})
+
 export default function Home() {
   return (
-    <>
-      <h1>Hello NextAuth.js</h1>
-    </>
+    <main className="h-full flex flex-col justify-center items-center
+      bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1 className={cn(
+          "text-6xl font-semibold text-white drop-shadow-md",
+          poppins.className
+        )}>🔐 NextAuth.js</h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">Sign In</Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
